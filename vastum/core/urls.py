1063d6258768d1c4dca78259b91a8749307e44ce
WireROPE <trojanvulgar@gmail.com>
2026-07-23 17:56:17 -0300

    a

--- vastum/core/urls.py
+++ vastum/core/urls.py
@@ -1,9 +1,12 @@
-from core import views
 from django.urls import path
-
-app_name = 'core'
+from . import views
 
 urlpatterns = [
     path('', views.video_list_view, name='video_list'),
     path('video/<int:pk>/', views.video_detail_view, name='video_detail'),
+    path('upload/', views.upload_video_view, name='upload_video'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('fund-xmr/', views.monero_payment_view, name='fund_xmr'),
 ]
